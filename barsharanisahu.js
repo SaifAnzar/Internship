df4634694e98898602b83aefad1afbfbd9cbbe85
Barsharani Sahu <barsharanisahu029@gmail.com>
2026-07-03 18:59:28 +0530

    Completed JavaScript Student Result Information System task

--- barsharanisahu.js
+++ barsharanisahu.js
@@ -1,36 +1,109 @@
-alert("Welcome to my JavaScript Task!");
-let name = "Barsharani Sahu";
-console.log("Name:", name);
-document.write("Name: " + name + "<br>");
-
+// Student Detail
+console.log("===== STUDENT RESULT SUMMARY =====");
+let studentName = "Barsharani Sahu";
+console.log("Student Name: " + studentName);
+const rollNumber = "24BCA013";
+console.log("Roll Number: " + rollNumber);
 let age = 20;
-console.log("Age:", age);
-document.write("Age: " + age + "<br>");
-
-const college = "Regional College of Management, Bhubaneswar";
-console.log("College:", college);
-document.write("College: " + college + "<br>");
-
+console.log("Age: " + age);
+const collegeName = "Regional College of Management, Bhubaneswar";
+console.log("College: " + collegeName);
 const course = "BCA";
-console.log("Course:", course);
-document.write("Course: " + course + "<br>");
-
+console.log("Course: " + course);
 let city = "Bhubaneswar";
-console.log("City:", city);
-document.write("City: " + city + "<br>");
-
+console.log("City: " + city);
 let isStudent = true;
-console.log("Is Student:", isStudent);
-document.write("Is Student: " + isStudent + "<br>");
 
-// Variables for different data types
-let stringData = "JavaScript";
-console.log(typeof stringData);
-let numberData = 100;
-console.log(typeof numberData);
-let booleanData = true;
-console.log(typeof booleanData);
-let nullData = null;
-console.log(typeof nullData);
-let undefinedData;
-console.log(typeof undefinedData);
+// Subject Marks
+
+let htmlMarks = 90;
+console.log("HTML Marks: " + htmlMarks);
+let cssMarks = 85;
+console.log("CSS Marks: " + cssMarks);
+let javascriptMarks = 95;
+console.log("JavaScript Marks: " + javascriptMarks);
+let databaseMarks = 88;
+console.log("Database Marks: " + databaseMarks);
+let gitMarks = 92;
+console.log("Git Marks: " + gitMarks);
+
+// Calculations
+
+let totalMarks = htmlMarks + cssMarks + javascriptMarks + databaseMarks + gitMarks;
+
+const maximumMarks = 500;
+
+let averageMarks = totalMarks / 5;
+
+console.log("Total Marks: " + totalMarks);
+console.log("Maximum Marks: " + maximumMarks);
+console.log("Average Marks: " + averageMarks);
+
+// Data Types
+
+let myString = "Biology";
+let myNumber = 100;
+let myBoolean = true;
+let myNull = null;
+let myUndefined;
+
+console.log("String: " + myString);
+console.log("Number: " + myNumber);
+console.log("Boolean: " + myBoolean);
+console.log("Null: " + myNull);
+console.log("Undefined: " + myUndefined);
+
+console.log("Type of String: " + typeof myString);
+console.log("Type of Number: " + typeof myNumber);
+console.log("Type of Boolean: " + typeof myBoolean);
+console.log("Type of Null: " + typeof myNull);
+console.log("Type of Undefined: " + typeof myUndefined);
+
+// Arithmetic Operators
+
+let a = 20;
+let b = 2;
+
+console.log("Addition: " + (a + b));
+console.log("Subtraction: " + (a - b));
+console.log("Multiplication: " + (a * b));
+console.log("Division: " + (a / b));
+console.log("Modulus: " + (a % b));
+
+a++;
+console.log("Increment: " + a);
+
+b--;
+console.log("Decrement: " + b);
+
+let x = 10;
+
+x += 5;
+console.log("After += : " + x);
+
+x -= 2;
+console.log("After -= : " + x);
+
+x *= 3;
+console.log("After *= : " + x);
+
+x /= 2;
+console.log("After /= : " + x);
+
+// Comparison Operators
+
+console.log("HTML > CSS : " + (htmlMarks > cssMarks));
+console.log("HTML < CSS : " + (htmlMarks < cssMarks));
+console.log("HTML >= CSS : " + (htmlMarks >= cssMarks));
+console.log("HTML <= CSS : " + (htmlMarks <= cssMarks));
+console.log("HTML == CSS : " + (htmlMarks == cssMarks));
+console.log("HTML === CSS : " + (htmlMarks === cssMarks));
+console.log("HTML != CSS : " + (htmlMarks != cssMarks));
+
+// == and ===
+
+let numberValue = 100;
+let stringValue = "100";
+
+console.log("Using == : " + (numberValue == stringValue));
+console.log("Using === : " + (numberValue === stringValue));
