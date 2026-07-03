4bd030a069fe925d745839adb16dd8cbd77f678c
Aparna Das <Aparna.Das@rcm.ac>
2026-07-03 18:27:43 +0530

    Added JavaScript assignment files

---
+++ Aparna.js
@@ -0,0 +1,49 @@
+alert("Welcome to JavaScript!");
+
+// Variables using let
+let name = "Aparna Das";
+console.log("Name:", name);
+
+let age = 19;
+console.log("Age:", age);
+
+let city = "Bhubaneswar";
+console.log("City:", city);
+
+// Variables using const
+const college = "Regional College Of Management";
+console.log("College:", college);
+
+const course = "BCA";
+console.log("Course:", course);
+
+const isStudent = true;
+console.log("Is Student:", isStudent);
+
+// Browser Output
+document.write("<h2>Student Details</h2>");
+
+document.write("Name: " + name + "<br>");
+document.write("Age: " + age + "<br>");
+document.write("College: " + college + "<br>");
+document.write("Course: " + course + "<br>");
+document.write("City: " + city + "<br>");
+document.write("Is Student: " + isStudent + "<br><br>");
+
+// Different Data Types
+// Print Data Types
+
+let myString = "Hello JavaScript";
+console.log(typeof myString);
+
+let myNumber = 100;
+console.log(typeof myNumber);
+
+let myBoolean = true;
+console.log(typeof myBoolean);
+
+let myNull = null;
+console.log(typeof myNull);
+
+let myUndefined;
+console.log(typeof myUndefined);
