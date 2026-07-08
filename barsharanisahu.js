73324e8bfb1b595007ff17b9411f59c8b982541a
Barsharani Sahu <barsharanisahu029@gmail.com>
2026-07-08 18:42:42 +0530

    Completed JavaScript Task 3 - Student Eligibility and Grade Checker

--- barsharanisahu.js
+++ barsharanisahu.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // alert("Welcome to my JavaScript Task!");
 // let name = "Barsharani Sahu";
 // console.log("Name:", name);
@@ -34,6 +35,9 @@
 // console.log(typeof nullData);
 // let undefinedData;
 // console.log(typeof undefinedData);
+=======
+/*
+>>>>>>> 33699d0 (Completed JavaScript Task 3 - Student Eligibility and Grade Checker)
 // Student Detail
 console.log("===== STUDENT RESULT SUMMARY =====");
 let studentName = "Barsharani Sahu";
@@ -142,4 +146,99 @@ let numberValue = 100;
 let stringValue = "100";
 
 console.log("Using == : " + (numberValue == stringValue));
+<<<<<<< HEAD
 console.log("Using === : " + (numberValue === stringValue));
+=======
+console.log("Using === : " + (numberValue === stringValue));*/
+
+// Student 
+
+const studentName = "Barsharani Sahu";
+console.log("Student Name: " + studentName);
+let age = 19;
+console.log("Age: " + age);
+
+let javascriptMarks = 85;
+console.log("JavaScript Marks: " + javascriptMarks);
+let htmlMarks = 78;
+console.log("HTML Marks: " + htmlMarks);
+
+let cssMarks = 82;
+console.log("CSS Marks: " + cssMarks);
+
+let attendancePercentage = 90;
+console.log("Attendance: " + attendancePercentage + "%");
+const isStudent = true;
+console.log("Is Student: " + isStudent);
+
+// 2. Voting Eligibility
+if (age >= 18) {
+    console.log("Eligible to Vote");
+} else {
+    console.log("Not Eligible to Vote");
+}
+
+
+// 3. Even or Odd 
+let number = 15;
+
+if (number % 2 == 0) {
+    console.log(number + " is Even");
+} else {
+    console.log(number + " is Odd");
+}
+
+// 4. Student Grade 
+let averageMarks = (javascriptMarks + htmlMarks + cssMarks) / 3;
+
+console.log("Average Marks: " + averageMarks);
+
+if (averageMarks >= 80) {
+    console.log("Grade A");
+} else if (averageMarks >= 60) {
+    console.log("Grade B");
+} else if (averageMarks >= 40) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
+
+
+// 5. Internship Eligibility Checker
+if (age >= 18 && attendancePercentage >= 75 && isStudent === true) {
+    console.log("Eligible for Internship");
+} else {
+    console.log("Not Eligible for Internship");
+}
+
+console.log("--------------------------------");
+
+// 6. Logical Operator Practice
+
+// AND (&&)
+console.log("AND Operator:");
+console.log(age >= 18 && attendancePercentage >= 75);
+
+// OR (||)
+console.log("OR Operator:");
+console.log(age >= 18 || attendancePercentage >= 95);
+
+// NOT (!)
+console.log("NOT Operator:");
+console.log(!isStudent);
+
+console.log("--------------------------------");
+
+// 7. Comparison Operators
+
+console.log("Comparison Operators");
+
+console.log("Age > 18 : " + (age > 18));
+console.log("Age < 18 : " + (age < 18));
+console.log("Age >= 18 : " + (age >= 18));
+console.log("Age <= 18 : " + (age <= 18));
+
+console.log("10 == '10' : " + (10 == "10"));
+console.log("10 === '10' : " + (10 === "10"));
+console.log("10 != 20 : " + (10 != 20));
+>>>>>>> 33699d0 (Completed JavaScript Task 3 - Student Eligibility and Grade Checker)
