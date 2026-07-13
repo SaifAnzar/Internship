7d435b106412fd17048b0e156610e097cf1f47ea
Diksha <diksha.gupta@rcm.ac>
2026-07-13 18:00:05 +0530

    clearing branch

--- 2.js
+++
@@ -1,7 +0,0 @@
-let age = 22;
-
-if (age >= 18) {
-    console.log("Eligible to Vote");
-} else {
-    console.log("Not Eligible to Vote");
-}
--- 5.js
+++
@@ -1,9 +0,0 @@
-let age = 22;
-let attendancePercentage = 90;
-const isStudent = true;
-
-if (age >= 18 && attendancePercentage >= 75 && isStudent) {
-    console.log("Eligible for Internship");
-} else {
-    console.log("Not Eligible for Internship");
-}
